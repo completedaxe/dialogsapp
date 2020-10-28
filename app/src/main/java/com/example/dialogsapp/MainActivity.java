b92ceb2dafcb996262dcaf30309e9bfc34bffe15
completedaxe <49436206+completedaxe@users.noreply.github.com>
2020-10-28 16:13:51 +0500

    Initial commit

---
+++ app/src/main/java/com/example/dialogsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dialogsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_main);
+    }
+}
